461d70377c5210c39010c797dd1cc53a9464fc2f
Hielke Walinga <hielkewalinga@gmail.com>
2024-08-19 14:31:15 +0200

    Allow to manually set created_by instead of always rely on CRUM.

--- src/booking/models.py
+++ src/booking/models.py
@@ -16,17 +16,22 @@ class Instrument(models.Model):
     timestamp_created = models.DateTimeField(auto_now_add=True)
     timestamp_modified = models.DateTimeField(auto_now=True)
 
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='instruments_created')
-    modified_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='instruments_modified')
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='instruments_created')
+    modified_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='instruments_modified')
 
     def save(self, *args, **kwargs):
         """Set the correct user for created_by and modified_by"""
         user = crum.get_current_user()
         if user and not user.pk:  # AnonymousUser
             user = None
+
         if not self.pk:
-            self.created_by = user
+            self.created_by = self.created_by or user
+
         self.modified_by = user
+
         super().save(*args, **kwargs)
 
 
@@ -34,15 +39,18 @@ class Booking(models.Model):
     """A booking made for an instrument."""
     name = models.TextField()
     description = models.TextField(blank=True, null=True)
-    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE, related_name='bookings')
+    instrument = models.ForeignKey(
+        Instrument, on_delete=models.CASCADE, related_name='bookings')
 
     timestamp_start = models.DateTimeField()
     timestamp_end = models.DateTimeField()
 
     timestamp_created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings_created')
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='bookings_created')
 
     def save(self, *args, **kwargs):
         """Set the created_by"""
-        self.user = crum.get_current_user()
+        if not self.created_by:
+            self.created_by = crum.get_current_user()
         super().save(*args, **kwargs)
